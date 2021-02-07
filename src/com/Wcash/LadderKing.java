dc05fd8428d8c6b45d16b92ca99c986874b56211
DoubleYouCash <wyattduber@gmail.com>
2021-02-07 00:19:14 -0600

    The Start of Something Beautiful

---
+++ src/com/Wcash/LadderKing.java
@@ -0,0 +1,17 @@
+package com.Wcash;
+
+import org.bukkit.plugin.java.JavaPlugin;
+
+public class LadderKing extends JavaPlugin {
+
+    @Override
+    public void onEnable() {
+
+    }
+
+    @Override
+    public void onDisable() {
+
+    }
+
+}
